4910ce4614366daa1c3824f687dc4cefaf690a0d
Robert Rochlin <robertrochlin@MacBook-Air.local>
2023-11-06 19:01:39 -0800

    latest changes

--- 875_KokoEatingBananas/cpp/Koko.cpp
+++ 875_KokoEatingBananas/cpp/Koko.cpp
@@ -2,19 +2,35 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+#include <cmath>
 
 class Solution {
 public:
     int minEatingSpeed(std::vector<int>& piles, int h) {
         std::sort(piles.begin(), piles.end());
         const int size = piles.size();
-        if (h<size*2) return piles[size*2-h-1];
-        else return 0;
+        int k;
+        if (h<size*2) k = piles[size*2-h-1];
+        else k=1;
+        while(!checkK(piles,h,(double)k)){
+            k++;
+        }
+        return k;
+    }
+
+    bool checkK(std::vector<int>& piles, int h, double k){
+        for(auto it=piles.rbegin(); it!= piles.rend(); it++){
+            h-=*it/k;
+            if (h<0) return false;
+        }
+        return true;
     }
 };
 
 int main(){
     Solution sol;
+    std::vector<int> single({1000000000});
+    std::cout<<sol.minEatingSpeed(single, 2)<<'\n';
     std::vector<int> piles({3,6,7,11});
     std::cout<<sol.minEatingSpeed(piles, 8)<<'\n';
     std::vector<int> piles2({30,11,23,4,20});
