d612ba907eaa657c3f960980295c6a904a7ca180
Robert Rochlin <robertrochlin@MacBook-Air.local>
2023-11-08 10:32:44 -0800

    adding other method for binary_seach.cpp

--- 40_BinarySearch/cpp/binary_search.cpp
+++ 40_BinarySearch/cpp/binary_search.cpp
@@ -15,4 +15,20 @@ public:
         }
         return -1;
     }
+
+    int search(std::vector<int>& nums, int target) {
+        int divisor = nums.size()/2;
+        auto it = nums.begin() + divisor;
+        if (divisor == 1) {
+            if (*it > target) it -= divisor;
+            else if (*it < target) it += divisor;
+        }
+        while(*it != target){
+            if (divisor<=1) return -1;
+            divisor = std::ceil(divisor / 2.0);
+            if (*it > target) it -= divisor;
+            else if (*it < target) it += divisor;
+        }
+        return it-nums.begin();
+    }
 };
