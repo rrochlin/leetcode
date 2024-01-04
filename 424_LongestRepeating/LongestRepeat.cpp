2ceaf28517246cab9fed9a8746b9da48652d7391
Robert <rrochlin@yahoo.com>
2024-01-04 03:39:27 -0800

    updating solution for 424

--- 424_LongestRepeating/LongestRepeat.cpp
+++ 424_LongestRepeating/LongestRepeat.cpp
@@ -3,33 +3,26 @@
 #include <vector>
 
 class Solution {
-    // inspired by the solution to 003 
 public:
     int characterReplacement(std::string s, int k) {
-        std::vector<int> numchars(91,0); // only need upper case english 65-90
-        int badchars=0, l=0, r=0;
+        std::vector<int> numchars(26,0); // only need upper case english
+        int  l=0, r=0;
         char mcc=s[0];
         while (r<s.length()){
-            numchars[s[r]]++;
-            if (numchars[mcc] < numchars[s[r]]){
-                badchars += numchars[mcc]-numchars[s[r]]+1;
-                mcc = s[r];
-            }
-            else if (s[r] != mcc) badchars++;
-            if (badchars > k){
-                numchars[s[l]]--;
-                if (s[l++]!=mcc) badchars--;
-            }
+            numchars[s[r]-'A']++;
+            // potentially losing time here with frequent max char swapping
+            if (numchars[mcc-'A'] < numchars[s[r]-'A']) mcc = s[r];
+            if (r-l+1-numchars[mcc-'A'] > k) numchars[s[l++]-'A']--;
             r++;
         }
         return r-l;
-    }
+        }
 };
 
 int main(){
   Solution sol;
 //   std::string s = "ABAB";
 //   std::cout<<sol.characterReplacement(s, 2)<<"\n";
-  std::string s2 = "ABCDE";
+  std::string s2 = "AABABBA";
   std::cout<<sol.characterReplacement(s2, 1)<<"\n";
 }; 
