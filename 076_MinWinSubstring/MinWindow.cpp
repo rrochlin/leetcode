6e3633b50a2bdd3b8b3a17ee7a23d7dac4c89228
Robert <rrochlin@yahoo.com>
2024-01-15 22:06:52 -0800

    starting 076

---
+++ 076_MinWinSubstring/MinWindow.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+#include <iostream>
+
+class Solution {
+public:
+    int maxProfit(std::vector<int>& prices) {
+        
+    }
+};
+
+
+int main(){
+    Solution sol;
+    std::vector<int> vec({1,2});
+    std::cout<<sol.maxProfit(vec)<<'\n';
+}; 
