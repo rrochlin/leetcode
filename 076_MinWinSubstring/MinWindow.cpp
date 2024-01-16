6e63e8b2c3769dfd6df2bb2a64871d5ba083b8e9
Robert <rrochlin@yahoo.com>
2024-01-16 07:06:47 -0800

    finished 076

--- 076_MinWinSubstring/MinWindow.cpp
+++ 076_MinWinSubstring/MinWindow.cpp
@@ -1,16 +1,36 @@
-#include <vector>
+#include <string>
 #include <iostream>
 
 class Solution {
 public:
-    int maxProfit(std::vector<int>& prices) {
-        
+    std::string minWindow(std::string s, std::string t) {
+        if (s.size()<t.size()) return "";
+        int l=0, r=0;
+        int minl=0, minr=100'001;
+        int count[128]={0}, unique=0;
+        for (int i=0; i<t.size(); i++) if(count[t[i]]++==0) unique++;
+        while (r<s.size()){
+            if (count[s[r++]]--==1) unique--;
+            if (unique==0) {
+                while (true)
+                    if (count[s[l++]]++==0) {unique++; break;}
+                if (r-l<minr-minl) minr=r, minl=l-1;
+            }
+        }
+        if (minr>100'000) return "";
+        return s.substr(minl, minr-minl);
     }
 };
 
 
 int main(){
     Solution sol;
-    std::vector<int> vec({1,2});
-    std::cout<<sol.maxProfit(vec)<<'\n';
+    std::string s = "ADOBECODEBANC", t = "ABC";
+    std::cout<<sol.minWindow(s,t)<<'\n';
+    s = "a", t = "a";
+    std::cout<<sol.minWindow(s,t)<<'\n';
+    s = "a", t = "aa";
+    std::cout<<sol.minWindow(s,t)<<'\n';
+    s = "cabwefgewcwaefgcf", t = "cae";
+    std::cout<<sol.minWindow(s,t)<<'\n';
 }; 
