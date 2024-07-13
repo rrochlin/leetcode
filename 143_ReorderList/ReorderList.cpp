f5e6dd95a6273dd52486c997183c039f28930cb9
robert rochlin <rrochlin@yahoo.com>
2024-07-12 20:33:52 -0700

    Finsihed Working on 143 added faster solution

--- 143_ReorderList/ReorderList.cpp
+++ 143_ReorderList/ReorderList.cpp
@@ -49,6 +49,34 @@ public:
         head->next = nullptr;
 
     }
+    void reorderListFast(ListNode* head) {
+            //ios_base::sync_with_stdio(false);
+            //cin.tie(nullptr);
+            ListNode* slow = head;
+            ListNode* fast = head->next;
+            while(fast && fast->next){
+                slow = slow->next;
+                fast = fast->next->next;
+            }
+            ListNode* second = slow->next;
+            ListNode* prev = slow->next = nullptr;
+            while(second!=nullptr){
+                ListNode* temp = second->next;
+                second->next = prev;
+                prev = second;
+                second = temp;
+            }
+            ListNode* first = head;
+            second = prev;
+            while(second!=nullptr){
+                ListNode* temp1 = first->next;
+                ListNode* temp2 = second->next;
+                first->next = second;
+                second->next = temp1;
+                first = temp1;
+                second = temp2;
+            }
+        }
 };
 
 
