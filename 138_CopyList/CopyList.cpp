554d9c4b583b0659c3672a14dadc298d12e873bd
robert rochlin <rrochlin@yahoo.com>
2024-07-21 20:42:31 -0700

    Finished 138

--- 138_CopyList/CopyList.cpp
+++ 138_CopyList/CopyList.cpp
@@ -2,13 +2,7 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
+#include <unordered_map>
 class Node {
 public:
     int val;
@@ -22,88 +16,110 @@ public:
     }
 };
 
-template< typename T, size_t N>
-ListNode* make_linked_list(T (&head)[N]){
-    std::reverse(std::begin(head), std::end(head));
-    ListNode* current;
-    ListNode* prev = nullptr;
-    ListNode* next = nullptr;
-    for (int i : head) {
-        current = new ListNode(i);
-        current->next = prev;
-        prev = current;
-    }
-    return current;
-}
-
 
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-
+        if (head == nullptr) return head;
+        Node* new_head = new Node(head->val);
+        Node* start = new_head;
+        std::vector<Node*> randoms;
+        std::unordered_map<Node*, int> order;
+        int i = 0;
+        std::vector<Node*> new_nodes;
+        new_nodes.push_back(new_head);
+        while(true) {
+            randoms.push_back(head->random);
+            order[head] = i++;
+            head = head->next;
+            if (head==nullptr) break;
+            new_head->next = new Node(head->val);
+            new_head = new_head -> next;
+            new_nodes.push_back(new_head);
+        }
+        order[nullptr] = i;
+        new_nodes.push_back(nullptr);
+        new_head = start;
+        for (Node* n : randoms) {
+            new_head->random = new_nodes[order.at(n)];
+            new_head = new_head->next;
+        }
+        return start;
     }
 
-    void reorderList(ListNode* head) {
-        ListNode* copy = head;
-        std::vector<ListNode*> tail;
-        int len = 0;
-        while (copy!=nullptr){
-            tail.push_back(copy);
-            copy = copy->next;
-            len++;
+    Node* copyRandomListLowMem(Node* head) {
+        if(head == NULL){
+            return NULL;
         }
-        len/=2;
-        ListNode* next;
-        auto it = tail.rbegin();
-        while (len>0){
-            next = head->next;
-            head->next = *it;
-            head->next->next = next;
-            head = next;
-            it++;
-            len--;
+
+        else{
+            // First, we construct a new list. We modify the next pointer of each node in the
+            // original list such that it points to the corresponding node in the new list.
+            std::vector<Node*> vec;
+            Node* newHead = NULL;
+            Node* newTail = NULL;
+            Node* tmp1 = head;
+            Node* tmp2 = NULL;
+            while(tmp1 != NULL){
+                vec.push_back(tmp1);
+                if(newTail == NULL){
+                    newHead = new Node(tmp1->val);
+                    newTail = newHead;
+                }
+                else{
+                    newTail->next = new Node(tmp1->val);
+                    newTail = newTail->next;
+                }
+
+                tmp2 = tmp1;
+                tmp1 = tmp1->next;
+                tmp2->next = newTail;
+            }
+
+            // Set the random pointer in the new list.
+            tmp1 = newHead;
+            for(int i=0; i<vec.size(); i++){
+                if(vec[i]->random != NULL){
+                    tmp1->random = vec[i]->random->next;
+                }
+                else{
+                    tmp1->random = NULL;
+                }
+
+                tmp1 = tmp1->next;
+            }
+
+            // Recover the original list
+            for(int i=0; i<vec.size()-1; i++){
+                vec[i]->next = vec[i+1];
+            }
+            vec[vec.size()-1]->next = NULL;
+
+            return newHead;
         }
-        head->next = nullptr;
+    }
+    Node* copyRandomListFast(Node* head) {
+        // return head;
+        Node* temp=head;
+        map<Node*, Node*> mpp;
+        while(temp!=NULL){
+            Node* newnode=new Node(temp->val);
+            mpp[temp]=newnode;
+            temp=temp->next;
+        }
+        temp=head;
+        while(temp!=NULL){
+            Node* copynode=mpp[temp];
+            copynode->next=mpp[temp->next];
+            copynode->random=mpp[temp->random];
+            temp=temp->next;
+        }
+        return mpp[head];
 
     }
-    void reorderListFast(ListNode* head) {
-            //ios_base::sync_with_stdio(false);
-            //cin.tie(nullptr);
-            ListNode* slow = head;
-            ListNode* fast = head->next;
-            while(fast && fast->next){
-                slow = slow->next;
-                fast = fast->next->next;
-            }
-            ListNode* second = slow->next;
-            ListNode* prev = slow->next = nullptr;
-            while(second!=nullptr){
-                ListNode* temp = second->next;
-                second->next = prev;
-                prev = second;
-                second = temp;
-            }
-            ListNode* first = head;
-            second = prev;
-            while(second!=nullptr){
-                ListNode* temp1 = first->next;
-                ListNode* temp2 = second->next;
-                first->next = second;
-                second->next = temp1;
-                first = temp1;
-                second = temp2;
-            }
-        }
 };
 
 
 int main(){
     Solution sol;
-    int nodes[4] = {1,2,3, 4};
-    ListNode* head = make_linked_list(nodes);
-    sol.reorderList(head);
-    while(head != nullptr) {
-        std::cout<<head->val<<" ";
-        head = head->next;
-    }
 };
