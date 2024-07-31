55ab5bfc4e7985eb9c331f273161514ea1bc6ff4
robert rochlin <rrochlin@yahoo.com>
2024-07-30 23:19:53 -0700

    Finished 146

--- 146_LRUCache/LRU.cpp
+++ 146_LRUCache/LRU.cpp
@@ -3,97 +3,85 @@
 #include <vector>
 #include <string>
 #include <unordered_map>
+#include "file_reader.h"
 
-struct ListNode {
+struct MyListNode {
+    int key;
     int val;
-    ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
+    MyListNode *next;
+    MyListNode *prev;
+    MyListNode() : key(-1) {}
+    MyListNode(int x) : key(x) {}
+    MyListNode(int x, int y) : key(x), val(y) {}
+    MyListNode(int x, int y, MyListNode* prev) : key(x), val(y), prev(prev) {}
+    MyListNode(int x, int y, MyListNode* prev, MyListNode* next) : key(x), val(y), prev(prev), next(next) {}
 };
 
 class LRUCache {
 public:
     LRUCache(int capacity) {
-        m_capacity = capacity;
+        m_capacity = capacity+1;
+        tail = new MyListNode(-1);
+        head = tail;
+        m_cache[-1] = tail;
+        head->prev = tail;
+        tail->next = head;
+    }
+
+    void set_head(MyListNode* temp){
+        if (head==temp) return;
+        if (tail==temp) tail = temp->next;
+        else temp->prev->next = temp->next;
+        temp->next->prev = temp->prev;
+        head->next = temp;
+        temp->prev = head;
+        temp->next = nullptr;
+        head = temp;
     }
 
     int get(int key) {
         auto el = m_cache.find(key);
         if (el == m_cache.end()) return -1;
-        if(tail != nullptr && head != nullptr) {
-            if (tail->val == key) {
-                ListNode* temp = tail;
-                head->next = tail;
-                tail = tail->next;
-                head = head->next;
-                head->next = nullptr;
-
-            } else if (head->val != key) {
-                ListNode *prev;
-                ListNode *temp = tail;
-                while (temp->val != key) {
-                    prev = temp;
-                    temp = temp->next;
-                }
-                prev->next = temp->next;
-                head->next = temp;
-                head = temp;
-                head->next = nullptr;
-            }
-        }
-        return el->second;
+        set_head(el->second);
+        return el->second->val;
     }
 
     void put(int key, int value) {
         auto el = m_cache.find(key);
-        if (tail!=nullptr && head!=nullptr && el != m_cache.end()) {
-            if (tail->val==key) {
-                head->next = tail;
-                tail = tail->next;
-                head = head->next;
-                head->next = nullptr;
-            }
-            else if (head->val!=key) {
-                ListNode* prev;
-                ListNode* temp = tail;
-                while(temp->val != key) {
-                    prev = temp;
-                    temp = temp->next;
-                }
-                prev->next = temp->next;
-                head->next = temp;
-                head = temp;
-                head->next = nullptr;
-            }
+        if (el != m_cache.end()) {
+            set_head(el->second);
+            head->val = value;
         }
         else {
-            if (tail==nullptr) tail = new ListNode(key);
-            else if (head==nullptr) {
-                head = new ListNode(key);
+            if (m_capacity==2){
+                m_cache.erase(tail->key);
+                auto temp = new MyListNode(key, value, head);
+                head = temp;
+                tail = temp;
                 tail->next = head;
+                head->next = tail;
+                m_cache[key] = head;
             }
             else {
-                head->next = new ListNode(key);
-                head = head->next;
+                if (m_capacity==m_cache.size()+1){
+                    m_cache.erase(tail->key);
+                    tail = tail->next;
+                    tail->prev = nullptr;
+                }
+                auto temp = new MyListNode(key, value, head);
+                head->next = temp;
+                head = temp;
+                m_cache[key] = head;
             }
         }
-        if (el == m_cache.end()) {
-            if (m_capacity > 0) m_capacity--;
-            else {
-                m_cache.erase(tail->val);
-                tail = tail->next;
-            }
-        }
-        m_cache[key] = value;
 
 
     }
 private:
     int m_capacity;
-    std::unordered_map<int, int> m_cache;
-    ListNode* tail = nullptr;
-    ListNode* head = nullptr;
+    std::unordered_map<int, MyListNode*> m_cache;
+    MyListNode* head = nullptr;
+    MyListNode* tail = nullptr;
 };
 
 /**
@@ -104,12 +92,15 @@ private:
  */
 
 int main(){
-//    std::string ins[] =  {"LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"};
-//    int nums[][2] = {{2},{1,1},{2,2},{1},{3,3},{2},{4,4},{1},{3},{4}};
+    std::string ins[] =  {"LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"};
+    int nums[][2] = {{2},{1,1},{2,2},{1},{3,3},{2},{4,4},{1},{3},{4}};
 //    std::string ins[] =  {"LRUCache","put","put","put","put","get","get"};
 //    int nums[][2] = {{2},{2,1},{1,1},{2,3},{4,1},{1},{2}};
-    std::string ins[] =  {"LRUCache","put","put","put","put","get","get","get","get","put","get","get","get","get","get"};
-    int nums[][2] = {{3},{1,1},{2,2},{3,3},{4,4},{4},{3},{2},{1},{5,5},{1},{2},{3},{4},{5}};
+//    std::string ins[] =  {"LRUCache","put","get","put","get","get"};
+//    int nums[][2] = {{1},{2,1},{2},{3,2},{2},{3}};
+
+//    std::vector<std::string> ins = readFileToStringVector("/Users/robertrochlin/Documents/Leetcode/146_LRUCache/input.txt");
+//    std::vector< std::vector<int> > nums = readFileToInt2DVector("/Users/robertrochlin/Documents/Leetcode/146_LRUCache/input2.txt");
     int j = 0;
     LRUCache* obj;
     int param_1;
